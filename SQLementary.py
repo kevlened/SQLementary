676a112494a2e3b116069b02134bcd193c2a3021
Len Boyette <boyettel@gmail.com>
2013-01-18 12:42:31 -0500

    Populate all tables and columns. Determine necessary tables.

--- SQLementary.py
+++ SQLementary.py
@@ -1,4 +1,9 @@
+from elm_objects import elm_column, elm_constraint, elm_table
+import sqlsoup
+from sqlalchemy import MetaData
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 def run(database_type, database_url, returned_columns, schema = None, username = None, password = None,  constraints = None, row_limit = None):
     '''
@@ -15,16 +20,71 @@ def run(database_type, database_url, returned_columns, schema = None, username =
     @param password: optional database credentials
     @type returned_columns: list[string]
     @param returned_columns: columns that are expected in the resulting rows
-    @type constraint: list[list[string,string,string,string=None]]
+    @type constraint: list[list[string,string,string,string]]
     @param constraint: optional columns to constrain. list[table.column,operator,val1,optional val2]
     @type row_limit: integer
     @param row_limit: optional number of rows returned
     @return: SQL as a string
     '''
+    
+    db_full_loc = '%s:///%s' % (database_type, database_url)
+    db = sqlsoup.SQLSoup(db_full_loc)
+    
+    elm_tables = []
+    elm_constraints = []
+    
+    logging.info('Pulling table and column Metadata from database')
+    if not len(db._metadata.tables):
+        db._metadata.reflect()
+    sqa_tables = db._metadata.tables.values()
+    
+    logging.info('Loading all tables and columns objects using Metadata')
+    for sqa_table in sqa_tables:
+        elm_tables.append(elm_table(sqa_table))
+    
+    logging.info('Building a list of constraint objects from constraint list argument')
+    if constraints:
+        for c in constraints:
+            '''Sanitize val2'''
+            if len(c) == 4:
+                c.append(None)
+            if c[4] == '':
+                c[4] = None
+            else:
+                raise Exception (c + ' is not a valid constraint')
+            
+            tab_col = c[0].split('.')
+            if len(tab_col) != 2:
+                raise Exception (str(c[0]) + ' is ambiguous or not in the right format')
+                                  
+            tab = tab_col[0]
+            col = tab_col[1]
+            op = c[2]
+            v1 = c[3]
+            v2 = c[4]
+            
+            elm_constraints.append(elm_constraint(tab,col,op,v1,v2))
+    
+    logging.info('Building a list of all necessary columns to find necessary tables')
+    ret_cols_temp = []        
+    for tab_col in returned_columns:
+        tc = tab_col.split('.')
+        if len(tc) != 2:
+            raise Exception (tab_col + ' is ambiguous or not in the right format')
+        ret_cols_temp.append([tc[0], tc[1]])
+        
+    tabs_temp = set([table_column[0] for table_column in ret_cols_temp])
+    con_tabs_temp = set([con.table_name for con in elm_constraints])
+    
+    tabs_temp = tabs_temp.union(con_tabs_temp)
+    
+    '''TODO: We have the names of the tables we need in tabs_temp. These need to have the optimal joins'''
+    
+    
     pass
 
 def main():
-    pass
+    run('sqlite','C:\Chinook_Sqlite.sqlite',['Track.Name', 'Artist.Name'])
 
 if __name__ == '__main__':
     main()
--- elm_objects.py
+++ elm_objects.py
@@ -1,37 +1,35 @@
 
-
 class elm_constraint(object):
     '''Represents a constraint on a particular column'''
-    def __init__(self, table_name, column_name, operator, val1, val2 = None):
+    def __init__(self, table_name, column_name, operator, val1, val2):
                 
-        self.table_name = None
+        self.table_name = table_name
         '''table_name (string) = table name of associated column'''
         
-        self.column_name = None
+        self.column_name = column_name
         '''column_name (string) = column name of associated column'''
         
-        self.operator = None
+        self.operator = operator
         '''operator (string) = boolean or comparative operators defining how to use val1 and val2'''
         
-        self.val1 = None
+        self.val1 = val1
         '''val1 (string) = first and primary value'''
         
-        self.val2 = None
+        self.val2 = val2
         '''val2 (string) = used for BETWEEN operator only'''
         pass
     
 class elm_column(object):
     '''Represents a column within SQLementary'''
-    def __init__(self, column_data):
-        
-        self.name = None
-        '''name (string) = column name'''
-        
-        self.parent_table = None
-        '''parent_table (string) = table name of the table this column belongs to'''
-        
+    def __init__(self, column_data):        
         self.data = column_data
         '''data (SQLAlchemy.column) = column as it's represented by SQLAlchemy'''
+        
+        self.name = self.data.name
+        '''name (string) = column name'''
+        
+        self.parent_table = self.data._from_objects[0].name
+        '''parent_table (string) = table name of the table this column belongs to'''
         
         self.constraint = None
         '''constraint (elm_constraint) = how the column should be constrained (operator & values)'''
@@ -40,13 +38,15 @@ class elm_column(object):
 class elm_table(object):
     '''Represents a table within SQLementary'''
     def __init__(self,table_data):
-        
-        self.name = None
-        '''name (string) = table name'''
-        
         self.data = table_data
         '''data (SQLAlchemy.table) = table as it's represented by SQLAlchemy'''
         
-        self.elm_columns = None
+        self.name = self.data.name
+        '''name (string) = table name'''
+        
+        self.elm_columns = []
         '''elm_columns (list[elm_column]) = all the columns in this tables'''
+        
+        for sqa_column in self.data._columns._all_cols:
+            self.elm_columns.append(elm_column(sqa_column))
         pass
