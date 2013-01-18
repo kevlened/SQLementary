97d96f57576b66dac33de311cb5b5d7892263bab
Len Boyette <boyettel@gmail.com>
2013-01-17 19:49:09 -0500

    Pretty docs

--- SQLementary.py
+++ SQLementary.py
@@ -2,13 +2,24 @@
 
 def run(database_type, database_url, returned_columns, schema = None, username = None, password = None,  constraints = None, row_limit = None):
     '''
-    database_type (string) = sqlite, oracle, db2 etc. from the list of acceptable databases in SQLalchemy
-    database_url (string) = location of the database
-    schema (string) = optional database schema
-    username (string), password (string) = optional database credentials
-    returned_columns (list[string]) = columns that are expected in the resulting rows
-    constraint (list[list[string,string,string,string=None]]) = optional columns to constrain. list[table.column,operator,val1,optional val2]
-    row_limit = optional number of rows returned
+    Returns SQL without the need to understand the SQL logic
+    @type database_type: string
+    @param database_type: sqlite, oracle, db2 etc. from the list of acceptable databases in SQLalchemy
+    @type database_url: string
+    @param database_url: location of the database
+    @type schema: string
+    @param schema: optional database schema
+    @type username: string
+    @param username: optional database credentials
+    @type password: string
+    @param password: optional database credentials
+    @type returned_columns: list[string]
+    @param returned_columns: columns that are expected in the resulting rows
+    @type constraint: list[list[string,string,string,string=None]]
+    @param constraint: optional columns to constrain. list[table.column,operator,val1,optional val2]
+    @type row_limit: integer
+    @param row_limit: optional number of rows returned
+    @return: SQL as a string
     '''
     pass
 
--- elm_objects.py
+++ elm_objects.py
@@ -3,51 +3,50 @@
 class elm_constraint(object):
     '''Represents a constraint on a particular column'''
     def __init__(self, table_name, column_name, operator, val1, val2 = None):
-        '''
-        table_name (string) = table name of associated column
-        column_name (string) = column name of associated column
-        operator (string) = boolean or comparative operators defining how to use val1 and val2
-        val1 (string) = first and primary value
-        val2 (string) = used for BETWEEN operator only
-        '''
-        #: table_name (string) = table name of associated column
+                
         self.table_name = None
-        #: column_name (string) = column name of associated column
+        '''table_name (string) = table name of associated column'''
+        
         self.column_name = None
-        #: operator (string) = boolean or comparative operators defining how to use val1 and val2
+        '''column_name (string) = column name of associated column'''
+        
         self.operator = None
-        #: val1 (string) = first and primary value
+        '''operator (string) = boolean or comparative operators defining how to use val1 and val2'''
+        
         self.val1 = None
-        #: val2 (string) = used for BETWEEN operator only
+        '''val1 (string) = first and primary value'''
+        
         self.val2 = None
+        '''val2 (string) = used for BETWEEN operator only'''
         pass
     
 class elm_column(object):
     '''Represents a column within SQLementary'''
     def __init__(self, column_data):
-        '''
-        column_data (SQLAlchemy.column) = column as it's represented by SQLAlchemy
-        '''
-        #: name (string) = column name
+        
         self.name = None
-        #: parent_table (string) = table name of the table this column belongs to
+        '''name (string) = column name'''
+        
         self.parent_table = None
-        #: data (SQLAlchemy.column) = column as it's represented by SQLAlchemy
+        '''parent_table (string) = table name of the table this column belongs to'''
+        
         self.data = column_data
-        #: constraint (elm_constraint) = how the column should be constrained (operator & values)    
+        '''data (SQLAlchemy.column) = column as it's represented by SQLAlchemy'''
+        
         self.constraint = None
+        '''constraint (elm_constraint) = how the column should be constrained (operator & values)'''
         pass
     
 class elm_table(object):
     '''Represents a table within SQLementary'''
     def __init__(self,table_data):
-        '''
-        table_data (SQLAlchemy.table) = table as it's represented by SQLAlchemy
-        '''
-        #: name (string) = table name
+        
         self.name = None
-        #: data (SQLAlchemy.table) = table as it's represented by SQLAlchemy
-        self.data = None
-        #: elm_columns (list[elm_column]) = all the columns in this tables
+        '''name (string) = table name'''
+        
+        self.data = table_data
+        '''data (SQLAlchemy.table) = table as it's represented by SQLAlchemy'''
+        
         self.elm_columns = None
+        '''elm_columns (list[elm_column]) = all the columns in this tables'''
         pass
