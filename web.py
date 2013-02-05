dcdb8dad72e87cee7eb86e152ced8d70b2f07de0
Len Boyette <boyettel@gmail.com>
2013-02-05 17:53:12 -0500

    Ready to deploy

--- web.py
+++ web.py
@@ -15,16 +15,13 @@ def index():
 @app.route('/sample<int:query_id>/schema', methods=['GET', 'POST'])
 def get_schema(query_id):
     if query_id == 1:
-        relchinookloc = url_for('static', filename='sample_databases/Chinook_Sqlite.sqlite')[1:]        
-        #relchinookloc = relchinookloc[1:] # Remove the leading forward slash
+        relchinookloc = url_for('static', filename='sample_databases/Chinook_Sqlite.sqlite')[1:] # Remove the leading forward slash        
         db, schema = build_elm_schema('%s:///%s' % ('sqlite', relchinookloc))
         schema_dict = {tab.name:[col.name for col in tab.elm_columns] for tab in schema}
         return json.dumps(schema_dict)
     
 @app.route('/sample<int:query_id>/query', methods=['GET', 'POST'])
 def get_query_data(query_id):
-    #desiredcols = json.loads(request.form['desiredcolumns'])
-    #filters = json.loads(request.form['filters'])
     data = request.json
     desiredcols = [col['table'] + '.' + col['column'] for col in data['desiredcolumns']]
     filters = [[fil['table'] + '.' + fil['column'], fil['operator'], fil['value1'], fil['value2']] for fil in data['filters']]
@@ -36,9 +33,8 @@ def get_query_data(query_id):
     return response
 
 if __name__ == "__main__":
-#    app.config['DEBUG'] = False
-#    # Start server
-#    port = int(os.environ.get("PORT", 5000))
-#    app.run('0.0.0.0', port)
-    app.debug = False
-    app.run()
+    # Start server    
+    app.debug = False    
+    #app.run()
+    port = int(os.environ.get("PORT", 80))
+    app.run('0.0.0.0', port)
